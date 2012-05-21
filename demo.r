f90fb2db5a2f6e98e6e50ad78a3c46f778c72b6c
Garrett <grolemund@gmail.com>
2012-05-21 17:14:09 -0500

    Uploading Hadley's comments on demo.r

--- demo.r
+++ demo.r
@@ -68,6 +68,85 @@ dgply(test.data,
 	.combine = fun(nest, "layers", mapping = aes(x = mean(Fertility), 
 		y = mean(Education)), width = 5, height = 5))
 
+dgply(test.data, c("lat", "long"),
+	fun(geom_point, "data", 
+		mapping = aes(x = Fertility, y = Education, color = rank(Catholic)), 
+		size = 3),
+	fun(nest, "layers", 
+		mapping = aes(x = mean(Fertility), y = mean(Education)), 
+		width = 5, height = 5))
+
+
+# adjusting for overlaps etc - separate step modifying the pos_major data frame
+# drawing in polar coordinates: adjusting pos_minor, converting from cartesian to polar
+#   (won't work for all geoms, but that's ok - will work for lines and points at least)
+dgply2 <- function(data, vars, geom, minor, major, ...) {
+	data$.gid <- id(data[vars])
+	
+	pos_major <- ddply(data, ".gid", function(df) {
+		data.frame(x = eval(major$x, df), y = eval(major$y, df))
+	})
+
+	geom <- match.fun(paste("geom_", geom, sep = ""))
+	minor$.gid <- quote(.gid)
+	plot <- ggplot_build(ggplot(data) + 
+		geom(minor) + 
+		facet_wrap(".gid"))
+	
+	plot$data[[1]]$PANEL <- 1L
+	plot$plot$facet <- facet_null()
+	grid.draw(ggplot_gtable(plot))
+
+
+	
+	browser()
+}
+
+
+# Specific examples
+# * glyph type: {point, line, bar, density, histogram} x {cartesian, polar}
+# * glyph position: spatial location, temporal location, arbitrary summary
+# * glyph position adjustment: various overlapping adjustments
+# * embedding: over all layers, over specific layer
+# * unique, rounded/binning
+
+# How do we do them currently?
+# What should the syntax look like? 
+#  embedded_layer(
+#    geom_point(aes(Education, Fertility)), 
+#    grid(c("lat", "long"), width = 5, height = 5)) ?
+# How should it be implemented? 
+
+
+
+# * time series of spatial
+# * scatterplot plot of spatial
+# * scatterplot with arbitrary summaries used for x and y position
+# * cleveland cycle plot 
+# * polar glyphs
+# * bar charts
+# * embedded polar bar charts
+# * density plots
+# * histograms
+
+dgply2(test.data, c("lat", "long"), geom = "point", 
+	minor = aes(Fertility, Education, colour = Agriculture),
+	major = aes(mean(Fertility), mean(Education)), 
+	width = 5, height = 5)
+
+dgply2(test.data, c("lat", "long"), geom = "histogram", 
+	minor = aes(Fertility),
+	major = aes(mean(Fertility), mean(Education)), 
+	width = 5, height = 5)
+
+dgply2(test.data, c("lat", "long"), geom = "histogram", 
+	minor = aes(Fertility),
+	major = aes(mean(Fertility), mean(Education)), 
+	width = 5, height = 5)
+
+
+
+
 # NOT WORKING - smooth won't work on majors that only give back one value per group
 # relocate data and add across group aes
 dgply(test.data,
