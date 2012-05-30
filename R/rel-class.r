8f72df4a5a6428c5e11f995a47a4e23cca641e1f
Garrett <grolemund@gmail.com>
2012-05-30 07:59:38 -0500

    clearing out directory for glyphmaps

--- R/rel-class.r
+++
@@ -1,8 +0,0 @@
-# Relative dimensions ------------------------------------------------------
-rel <- function(x) {
-  structure(x, class = "rel")
-}
-
-print.rel <- function(x, ...) print(noquote(paste(x, " *", sep = "")))
-
-is.rel <- function(x) inherits(x, "rel")
--- README.md
+++
@@ -1,4 +0,0 @@
-ggplyr
-======
-
-split data with functions, transform pieces into plot layers, combine in a variety of ways
