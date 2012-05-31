b79f5a2a0d36b8e7689f41b8e2e6895e730ac322
garrettgman <grolemund@gmail.com>
2012-05-31 13:20:17 -0500

    Changed ply_aes to a generic S3 function

--- R/ply_aes.r
+++ R/ply_aes.r
@@ -1,10 +1,18 @@
 #' ply_aes causes the aesthetics of a layer to be computed groupwise.
 ply_aes <- function(layer, .vars = NULL) {
-  layer <- layer_clone(layer)
-  if (is.glayer(layer)) {
-    layer$compute_aesthetics <- plyr_aesthetics
-    return(layer)
-  }
+  UseMethod("ply_aes")
+}
+
+ply_aes.list <- function(layer, .vars = NULL) {
+  lapply(layer, ply_aes, .vars)
+}
+
+ply_aes.glayer <- function(layer, .vars = NULL) {
+  layer$compute_aesthetics <- plyr_aesthetics
+  layer
+}
+  
+ply_aes.proto <- function(layer, .vars = NULL) {
   if (!is.null(.vars)) {
     if (!is.function(.vars)) {
       .vars <- group_by(.vars)
