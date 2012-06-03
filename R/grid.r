7ed59503c2b3fe0be64cb93fabc36d8a08af4f4b
Garrett <grolemund@gmail.com>
2012-06-03 16:59:15 -0500

    fixed bug in grid/ply_aes interaction

--- R/grid.r
+++ R/grid.r
@@ -6,9 +6,9 @@ grid <- function(layer, grid.aes = aes(), x.nbin = 10, y.nbin = 10,
   if (is.glayer(layer) || is.list(layer)) {
     stop("Cannot grid glayer", call. = TRUE)
   }
-  if ("plyr" %in% ls(layer)) {
-    stop("grid and ply_aes used in wrong order.", call. = FALSE)
-  }
+ # if ("plyr" %in% ls(layer)) {
+  #  stop("grid and ply_aes used in wrong order.", call. = FALSE)
+  #}
   
   # aes
   if(is.null(grid.aes$x)) {
@@ -29,7 +29,7 @@ grid <- function(layer, grid.aes = aes(), x.nbin = 10, y.nbin = 10,
   layer$embed <- list(x_scale = x_scale, y_scale = y_scale,
     major.aes = grid.aes[c("x", "y")], grid_env = parent.frame(),
     x.nbin = x.nbin, y.nbin = y.nbin, w.adjust = width.adjust, 
-    h.adjust = height.adjust)
+    h.adjust = height.adjust, ref = .ref)
   layer$assign_glyphs <- assign_grid
   layer$combine_glyphs <- combine_glyphs
   if (.ref) layer$combine_glyphs <- combine_refs
@@ -51,14 +51,15 @@ assign_grid <- function(., data) {
   x.range <- range(x)
   x.breaks <- seq(x.range[1], x.range[2], length = embed$x.nbin + 1)
   width <- diff(x.range) / embed$x.nbin * embed$w.adjust
-  x.val <- (x.breaks + width/2)[-(embed$x.nbin + 1)]
+  # positions in bottom left corner, not on ref box line
+  x.val <- (x.breaks + (1 + 0.05 * !embed$ref) * width/2)[-(embed$x.nbin + 1)]
   x.glyph <- cut(x, x.breaks, labels = x.val, include.lowest = TRUE)
   
   y <- eval(embed$major.aes$y, envir = data, enclos = embed$grid_env)
   y.range <- range(y)
   y.breaks <- seq(y.range[1], y.range[2], length = embed$y.nbin + 1)
   height <- diff(y.range) / embed$y.nbin * embed$h.adjust
-  y.val <- (y.breaks + height/2)[-(embed$y.nbin + 1)]
+  y.val <- (y.breaks + (1 + 0.05 * !embed$ref) * height/2)[-(embed$y.nbin + 1)]
   y.glyph <- cut(y, y.breaks, labels = y.val, include.lowest = TRUE)
   
   data$GLYPH <- as.numeric(interaction(x.glyph, y.glyph, drop = TRUE))
